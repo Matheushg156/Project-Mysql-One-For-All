54d5d13c03577e689bcc5364e985f99802581615
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-01-10 21:41:47 -0300

    Requesito 06

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT 
+    MIN(PL.plan_price) AS faturamento_minimo,
+    MAX(PL.plan_price) AS faturamento_maximo,
+    ROUND(AVG(PL.plan_price), 2) AS faturamento_medio,
+    SUM(PL.plan_price) AS faturamento_total
+FROM
+    SpotifyClone.plans AS PL
+        JOIN
+    SpotifyClone.users AS U ON U.user_plan = PL.plan_id;
